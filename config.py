49af70d97c741862e2b3e17f43ee8d9137bc982e
RFlintstone <rubenflint@hotmail.com>
2017-12-10 21:32:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 client_id   = 'Ch8pNOBOAYzASwSmKClKZJKts8VnKDsj' # your bot's client ID
 token = 'Mzc3MDU4MTc2MzUwMDI3Nzc3.DQ8csA.YRTHtBKENVulI8bk17-6gzGnScU' # your bot's token
 carbon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiIyMjA5NTkxNzg3NzkyNjI5ODYiLCJyYW5kIjozOTksImlhdCI6MTUxMjkzNTQ5NH0.1Al7nXx5HmvA7Lyc_ddc6V_czVxvD5K7dxPdrW0D1yE' # your bot's key on carbon's site
-bots_key = 'https://discordapp.com/oauth2/authorize?client_id=377058176350027777&scope=bot&permissions=805824638&response_type=code' # your key on bots.discord.pw
+bots_key = 'https://discordapp.com/oauth2/authorize?client_id=377058176350027777&scope=bot&permissions=268954750&response_type=code' # your key on bots.discord.pw
 postgresql = 'postgres://sflxeqaxuvrkro:d9df02d2b497e0f23c6663de06cbf5a17a7f1a867e7a9a6208484d292cb57f61@ec2-54-195-248-0.eu-west-1.compute.amazonaws.com:5432/d4a6dsf7p8cp8e' # your postgresql info from above
 challonge_api_key = 'bU7HgqNbDQhVysppOgPp8zoRqGE82cWf2iM0Q2Ep' # for tournament cog
