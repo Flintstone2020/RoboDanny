6ed42f4945430f6c1ee8a4c80abd2571dc04815a
RFlintstone <rubenflint@hotmail.com>
2017-12-10 21:09:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ client_id   = 'Ch8pNOBOAYzASwSmKClKZJKts8VnKDsj' # your bot's client ID
 token = 'Mzc3MDU4MTc2MzUwMDI3Nzc3.DQ8csA.YRTHtBKENVulI8bk17-6gzGnScU' # your bot's token
 #carbon_key = '' # your bot's key on carbon's site
 bots_key = 'https://discordapp.com/oauth2/authorize?client_id=377058176350027777&scope=bot&permissions=805314622&response_type=code' # your key on bots.discord.pw
-postgresql = 'postgres://haxjpdab:N3z6DS6u8_3SVgSlH_F88Zpkz5itAm_I@horton.elephantsql.com:5432/haxjpdab' # your postgresql info from above
+postgresql = 'postgres://sflxeqaxuvrkro:d9df02d2b497e0f23c6663de06cbf5a17a7f1a867e7a9a6208484d292cb57f61@ec2-54-195-248-0.eu-west-1.compute.amazonaws.com:5432/d4a6dsf7p8cp8e' # your postgresql info from above
 challonge_api_key = 'bU7HgqNbDQhVysppOgPp8zoRqGE82cWf2iM0Q2Ep' # for tournament cog
